a72b1aa5ce7169c99599e16e3a5bc528e0a2cab1
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2023-02-06 08:51:20 -0500

    Avanzando 3 el Front Menu Responsive OK

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -100,7 +100,7 @@ const NavContainer = styled.nav`
 const BgDiv = styled.div`
   background-color: #222;
   position: absolute;
-  top: -1000px;
+  top: -2000px;
   left: -1000px;
   width: 100%;
   height: 100%;
